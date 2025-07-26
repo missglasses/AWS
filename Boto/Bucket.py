14211c5d167e171327b7ef91e4d29de5ebf7c993
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-26 12:54:15 +0800

    Create Bucket.py

---
+++ Boto/Bucket.py
@@ -0,0 +1,13 @@
+import boto3
+
+# create boto3 client to S3
+s3 = boto3.client('s3', region_name='us-east-1', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+#syntax: responseObject = clientObject.createBucket(Bucket='')
+response_staging = s3.create_bucket(Bucket='gim-staging')
+response_processed = s3.create_bucket(Bucket='gim-processed')
+response_test = s3.create_bucket(Bucket='gim-test')
+
+print(response_staging)
